5098f8a9f96810f997ffd7d3d7e65dc06aa84fc5
LLevente0 <ladanyi.levente@diak.szbi-pg.hu>
2025-01-27 09:18:43 +0100

    Create 05-sort_vs_sorted.py

---
+++ 05-sort_vs_sorted.py
@@ -0,0 +1,18 @@
+L = [1, 5, 4, 2, 3]
+
+#Print the sorted list
+#print("Sorted list:")
+#print(sorted(L))
+
+#Print the original list
+#print("\nOriginal list after sorting:")
+#print(L)
+
+print(L)
+print(sorted(L))
+print(f"Original list: {L}" )
+print(L)
+
+
+L.sort()
+print(L)
